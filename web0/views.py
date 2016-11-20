35587918154c607737549ac0efe5a9d36e0a2fb3
Ronald Albert <ronaldalbert1609@gmail.com>
2016-11-20 12:57:54 -0200

    ImprovedMyKnowledgeOfHowToWorkingWithDatabases
    Connecting tables in databases and improved the details view

--- web0/views.py
+++ web0/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Album
 # Create your views here.
 def index(resquest):
 	all_albums = Album.objects.all()
-	html = ''
-	for album in all_albums:
-		url = '/web0/' + str(album.id) + '/'
-		html += '<a href="' + url + '">' + album.album_title + '</a></br>' 	
-	return HttpResponse(html)
+	ctx = {'all_albums': all_albums}
+	return render(resquest, 'web0/index.html', ctx)
 
 
 def detail(resquest, album_id):
-	return HttpResponse("<h2>Details for Album id: "+str(album_id)+"</h2>")
+	try:
+		album = Album.objects.get(id=album_id)
+		song = album.song_set.all()[0]
+		ctx = {'album': album, 'song': song}
+	except Album.DoesNotExist:
+		raise Http404("The album either has been deleted or never existed")
+	return render(resquest, 'web0/detail.html', ctx)
 
