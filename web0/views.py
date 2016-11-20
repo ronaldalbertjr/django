652b22b85522cc443485cb19fe3bca20651dbd2a
Ronald Albert <ronaldalbert1609@gmail.com>
2016-11-20 09:07:16 -0200

    AddedANewView
    Really enjoying to work with django pretty good framework

--- web0/views.py
+++ web0/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
 # Create your views here.
 def index(resquest):
-	return HttpResponse("<h1> This the music app homepage </h1>")
+	all_albums = Album.objects.all()
+	html = ''
+	for album in all_albums:
+		url = '/web0/' + str(album.id) + '/'
+		html += '<a href="' + url + '">' + album.album_title + '</a></br>' 	
+	return HttpResponse(html)
+
+
+def detail(resquest, album_id):
+	return HttpResponse("<h2>Details for Album id: "+str(album_id)+"</h2>")
+
