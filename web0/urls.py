6947c7307512006a9aae2493e9f1fdf0ee27303b
Ronald Albert <ronaldalbert1609@gmail.com>
2016-11-23 22:09:20 -0200

    ChangedMyViewsToClasses
    Instead of working iwth definitions now i'm workin with classes in views

--- web0/urls.py
+++ web0/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = "web0"
 urlpatterns = [
-	# /music/
-    url(r'^$', views.index, name = 'index'),
+	# /web0/
+    url(r'^$', views.IndexView.as_view(), name = 'index'),
 
-    # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    # /web0/<album_id>/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/<album_id>/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    # /web0/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 ]
--- web0/views.py
+++ web0/views.py
@@ -1,30 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Album
+from .models import Album, Song
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
-def index(resquest):
-	all_albums = Album.objects.all()
-	ctx = {'all_albums': all_albums}
-	return render(resquest, 'web0/index.html', ctx)
 
+class IndexView(generic.ListView):
+	template_name = 'web0/index.html'
+	context_object_name = 'all_albums'
 
-def detail(resquest, album_id):
-	album = get_object_or_404(Album,id=album_id)
-	song = album.song_set.get(is_favorite=True)
-	ctx = {'album': album, 'song': song}
-	return render(resquest, 'web0/detail.html', ctx)
+	def get_queryset(self):
+		return Album.objects.all()
 
-def favorite(resquest, album_id):
-	album = get_object_or_404(Album, id=album_id)
-	try:
-		selected_song = album.song_set.get(pk=resquest.POST['song'])
-	except (KeyError, Song.DoesNotExist):
-		return render(resquest, 'web0/detail.html' ,{'album': album, 'error_message': 'You did not select a valid song',})
-	else:
-		for song in album.song_set.all():
-			song.is_favorite = False
-			song.save() 
-		selected_song.is_favorite = True
-		selected_song.save()
-		ctx = {'album':album, 'song': selected_song}
-		return render(resquest, 'web0/detail.html', ctx)
+class DetailView(generic.DetailView):
+	model = Album
+	template_name = "web0/detail.html"
+
+class  AlbumCreate(CreateView):
+	model = Album
+	fields = ['artist', 'album_title', 'genre', 'album_logo']
+		
+
+		
+
